cff25edc42b553aa8d9a3be217c85889772c27d9
Mohamed <mohamedhema2017@gmail.com>
2023-04-22 23:24:28 +0200

    updateing calculator

--- Web Development BootCamp 2023/BMI Calculator/calculator.js
+++ Web Development BootCamp 2023/BMI Calculator/calculator.js
@@ -1,12 +1,29 @@
 //Require express in your calculator.js
-const express = require('express')
+const express = require('express');
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = 3000;
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 
 //Create a root route get method with app.get()
 app.get('/', (req, res) => {
     res.sendFile(__dirname+"/index.html");
-  })
+})
+
+app.post("/", function(req, res){
+
+    //convert text to number with "+" or Number(String)
+
+    var num1 = +req.body.n1;
+    var num2 = +req.body.n2;
+
+    var result = num1 + num2;
+
+    res.send("The result of the calculation is "+ result);
+})
 
 //Spin up our server on port 3000 with app.listen
   app.listen(port, () => {
