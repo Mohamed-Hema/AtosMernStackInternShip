eb97fdeceaf9b59a0d54358626034cb4d5a0a634
Mohamed <mohamedhema2017@gmail.com>
2023-04-22 23:43:18 +0200

    finished bmi calculator

--- Web Development BootCamp 2023/BMI Calculator/calculator.js
+++ Web Development BootCamp 2023/BMI Calculator/calculator.js
@@ -1,13 +1,14 @@
-//Require express in your calculator.js
+//Global Variables
 const express = require('express');
 const bodyParser = require("body-parser");
+const { dirname, parse } = require('path');
 
 const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-
+// Start Normal Calculator
 //Create a root route get method with app.get()
 app.get('/', (req, res) => {
     res.sendFile(__dirname+"/index.html");
@@ -16,13 +17,28 @@ app.get('/', (req, res) => {
 app.post("/", function(req, res){
 
     //convert text to number with "+" or Number(String)
-
-    var num1 = +req.body.n1;
-    var num2 = +req.body.n2;
+    var num1 = +req.body.num1;
+    var num2 = +req.body.num2;
 
     var result = num1 + num2;
 
     res.send("The result of the calculation is "+ result);
+})
+
+// Start BMI Calculator
+
+app.get("/bmiCalculator", function(req, res){
+  res.sendFile(__dirname + "/bmiCalculator.html")
+})
+
+app.post("/bmiCalculator", function(req, res){
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+
+  var bmi = weight / Math.pow(height, 2);
+
+  res.send("Your BMI is "+ bmi);
+
 })
 
 //Spin up our server on port 3000 with app.listen
