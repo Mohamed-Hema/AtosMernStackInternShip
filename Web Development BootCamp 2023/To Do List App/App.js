1cf10f1a0020fd312c01e1183b1a36b8f750bdef
Mohamed <mohamedhema2017@gmail.com>
2023-04-26 15:14:20 +0200

    upload

--- Web Development BootCamp 2023/To Do List App/App.js
+++ Web Development BootCamp 2023/To Do List App/App.js
@@ -4,7 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
- 
+
+app.set('view engine', 'ejs');
+
+
+
 app.get("/", function(req, res){
    var today = new Date();
    var currentDay = today.getDay();
@@ -12,9 +16,7 @@ app.get("/", function(req, res){
    if(currentDay === 6 || currentDay === 0){
     res.write("<h1>Yay it's the weekend!</h1>");
    } else {
-    res.write("<p>It is not the weekend</p>");
-    res.write("<h1>Boo! I have to work!</h1>");
-    res.send();
+    res.sendFile(__dirname + "/index.html");
    }
 });
 
