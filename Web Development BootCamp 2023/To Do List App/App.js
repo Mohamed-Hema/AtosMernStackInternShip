3580a8cbf0a02d54eae956d82a1c2801495e2e54
Mohamed <mohamedhema2017@gmail.com>
2023-04-26 22:50:32 +0200

    Boss Challenge 3

--- Web Development BootCamp 2023/To Do List App/App.js
+++ Web Development BootCamp 2023/To Do List App/App.js
@@ -12,11 +12,14 @@ app.set('view engine', 'ejs');
 app.get("/", function(req, res){
    var today = new Date();
    var currentDay = today.getDay();
+   var day = "";
    
    if(currentDay === 6 || currentDay === 0){
-    res.write("<h1>Yay it's the weekend!</h1>");
+    day = "Weekend";
+    res.render("List", {KindOfDay: day})
    } else {
-    res.sendFile(__dirname + "/index.html");
+    day = "Weekday";
+
    }
 });
 
