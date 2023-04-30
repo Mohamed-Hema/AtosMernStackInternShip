8e324e91d04ba50d76cbaf65dcff0ac94c3c3aae
Mohamed <mohamedhema2017@gmail.com>
2023-04-30 15:52:50 +0300

    blog

--- Web Development BootCamp 2023/To Do List App/App.js
+++ Web Development BootCamp 2023/To Do List App/App.js
@@ -8,21 +8,23 @@ const app = express();
 app.set('view engine', 'ejs');
 
 
+app.get("/", function(req, res) {
 
-app.get("/", function(req, res){
-   var today = new Date();
-   var currentDay = today.getDay();
-   var day = "";
-   
-   if(currentDay === 6 || currentDay === 0){
-    day = "Weekend";
-    res.render("List", {KindOfDay: day})
-   } else {
-    day = "Weekday";
+var today = date.getDate();
+  var today = new Date();
 
-   }
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+  var day = today.toLocalString("en-US", options);
+  res.render("List", {
+    kindOfDay: day
+  });
+
 });
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000.");
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
