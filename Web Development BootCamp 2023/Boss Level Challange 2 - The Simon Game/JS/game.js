cf51361f43d6ff9c16d713e39a407318bc00e999
Mohamed <mohamedhema2017@gmail.com>
2023-04-30 23:20:41 +0300

    new Projects added

--- Web Development BootCamp 2023/Boss Level Challange 2 - The Simon Game/JS/game.js
+++ Web Development BootCamp 2023/Boss Level Challange 2 - The Simon Game/JS/game.js
@@ -1,80 +1,75 @@
-// Define an array of button colors
-var buttonColours = ["red", "blue", "green", "yellow"];
-
+// Create empty arrays to hold data
 var gamePattern = [];
 var userClickedPattern = [];
+// Create an array with the available button colors
+var buttonColors = ["red", "blue", "green", "yellow"];
 
-// Set the initial state of the game
+// Set initial variables for the game
 var started = false;
 var level = 0;
 
-// Start the game when A key is pressed
+// Start the game
 $(document).keypress(function() {
   if (!started) {
-    // Update Level
     $("#level-title").text("Level " + level);
-    // Generate the next color sequence
     nextSequence();
     started = true;
   }
 });
 
-// Handle button clicks
 $(".btn").click(function() {
-  var userChosenColour = $(this).attr("id");
 
-  userClickedPattern.push(userChosenColour);
-  playSound(userChosenColour);
-  animatePress(userChosenColour);
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
 
-  checkAnswer(userClickedPattern.length-1);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+
+  checkAnswer(userClickedPattern.length - 1);
 });
 
-// Check if user input is like game input
+// Check if the user's clicked pattern matches the game pattern so far
 function checkAnswer(currentLevel) {
 
-  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-    // if they're all matched
-    if (userClickedPattern.length === gamePattern.length){
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+      
+// If the user's clicked pattern does not match the game pattern so far...
+    } else {
+      playSound("wrong");
+      $("body").addClass("game-over");
+      $("#level-title").text("Game Over, Press Any Key to Restart");
 
       setTimeout(function () {
-        nextSequence();
-      }, 1000);
+        $("body").removeClass("game-over");
+      }, 200);
+
+      startOver();
     }
-  } else { 
-    // if it doesn't match
-    playSound("wrong");
-    $("body").addClass("game-over");
-    $("#level-title").text("Game Over, Press Any Key to Restart");
-
-    //Game OVER!
-    setTimeout(function () {
-      $("body").removeClass("game-over");
-    }, 200);
-
-    startOver();
-  }
 }
 
-// Generate the next color sequence
+// Generate the next sequence in the game
 function nextSequence() {
-
   userClickedPattern = [];
   level++;
-
-  // Update Level
   $("#level-title").text("Level " + level);
-  // Generate a random number and use it to select a random color from the button colors array
   var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColour = buttonColours[randomNumber];
-  // Add the random color to the game pattern
-  gamePattern.push(randomChosenColour);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
 
-  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-  playSound(randomChosenColour);
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColor);
 }
 
-// Button Press Animation
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
 function animatePress(currentColor) {
   $("#" + currentColor).addClass("pressed");
   setTimeout(function () {
@@ -82,13 +77,6 @@ function animatePress(currentColor) {
   }, 100);
 }
 
-// Playing Button Sound
-function playSound(name) {
-  var audio = new Audio("sounds/" + name + ".mp3");
-  audio.play();
-}
-
-// Reset the game
 function startOver() {
   level = 0;
   gamePattern = [];
