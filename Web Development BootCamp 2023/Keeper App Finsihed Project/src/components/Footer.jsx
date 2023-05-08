9b066980f3c536203f06ccb5d4da20511c0553e8
Mohamed <mohamedhema2017@gmail.com>
2023-05-08 18:27:58 +0300

    uploading

--- Web Development BootCamp 2023/Keeper App Finsihed Project/src/components/Footer.jsx
+++ Web Development BootCamp 2023/Keeper App Finsihed Project/src/components/Footer.jsx
@@ -3,9 +3,11 @@ import React from "react";
 function Footer() {
   const year = new Date().getFullYear();
   return (
+    <>
     <footer>
       <p>Copyright ⓒ {year}, Made with ❣️ <a href="https://github.com/Mohamed-Hema" target="_blank">Mohamed Hema</a></p>
     </footer>
+    </>
   );
 }
 
